5855334121a440a77f259b6c6223ab93ac78275b
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-05-16 10:38:05 -0700

    more change

--- football PROJECT/Defender.java
+++ football PROJECT/Defender.java
@@ -10,25 +10,8 @@ import java.util.ArrayList;
 public class Defender extends Entity
 {
     public Defender(){
-        
         setImage("enemy.png");
     }
-    
-    public void fire(){
-        if (reloadDelayCount == getReloadTime()){
-            Bullet bullet = new Bullet(type,atk,getMovement().copy(), getRotation());
-            getWorld().addObject(bullet, getX(), getY());
-            bullet.move ();
-            reloadDelayCount = 0;
-        }
-    }
-    public void isDead(){
-        if(getHealth()<=0){
-            setImage("dead.png");
-            getEnemyArray().remove(
-        }
-    }
-    
     public void act() 
     {
         isDead();
